64f3766d652f05211fb9abda69cb5a6fc31c35ae
amit-kumaryadav <amtydv630@gmail.com>
2021-03-04 16:31:17 +0530

    Added logger

--- src/protectedApi_v8/user/profile-registry.ts
+++ src/protectedApi_v8/user/profile-registry.ts
@@ -343,10 +343,13 @@ profileRegistryApi.post('/createUserRegistryV2/:userId', async (req, res) => {
 })
 
 export async function getProfileStatus(userId: string) {
+  logInfo('Calling..... Profile Status')
   try {
     const response = await axios.get(API_END_POINTS.getUserRegistryById(userId), {
       ...axiosRequestConfig,
     })
+    logInfo('Got Response from registery')
+    logInfo(JSON.stringify(response))
     const userProfileResult = response.data.result.UserProfile
     if ((typeof userProfileResult !== 'undefined' && userProfileResult.length > 0) && (userId === userProfileResult[0].userId)) {
       let profileMatch = true

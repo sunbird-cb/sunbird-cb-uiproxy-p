abf48474d6f9f9dace85acc098548924945ae906
amit-kumaryadav <amtydv630@gmail.com>
2021-03-30 12:28:44 +0530

    Added frac api's

--- src/protectedApi_v8/frac.ts
+++ src/protectedApi_v8/frac.ts
@@ -6,8 +6,11 @@ import { CONSTANTS } from '../utils/env'
 
 const API_END_POINTS = {
     addDataNode: `${CONSTANTS.FRAC_API_BASE}/api/frac/addDataNode`,
-    getCompetencyArea: `${CONSTANTS.FRAC_API_BASE}/api/frac/getAllNodes?type=COMPETENCYAREA`,
+    addDataNodeBulk: `${CONSTANTS.FRAC_API_BASE}/api/frac/addDataNodeBulk`,
+    getActivity: `${CONSTANTS.FRAC_API_BASE}/api/frac/getAllNodes?type=ACTIVITY&status=VERIFIED`,
+    getCompetencyArea: `${CONSTANTS.FRAC_API_BASE}/api/frac/getAllNodes?type=COMPETENCYAREA&status=VERIFIED`,
     getDictionary: `${CONSTANTS.FRAC_API_BASE}/api/frac/getAllNodes?type=COMPETENCY&status=VERIFIED`,
+    getRole: `${CONSTANTS.FRAC_API_BASE}/api/frac/getAllNodes?type=ROLE&status=VERIFIED`,
     searchNodes: `${CONSTANTS.FRAC_API_BASE}/api/frac/searchNodes`,
 }
 
@@ -24,6 +27,12 @@ fracApi.get('/getAllNodes/:type', async (req, res) => {
                 break
             case 'competencyarea':
                 apiEndPoint = API_END_POINTS.getCompetencyArea
+                break
+            case 'role':
+                apiEndPoint = API_END_POINTS.getRole
+                break
+            case 'activity':
+                apiEndPoint = API_END_POINTS.getActivity
                 break
             default:
                 res.status(400).send('TYPE_IS_NOT_PROVIDED_OR_TYPE_IS_NOT_CONFIGURED')
@@ -48,6 +57,24 @@ fracApi.get('/getAllNodes/:type', async (req, res) => {
 fracApi.post('/addDataNode', async (req, res) => {
     try {
         const response = await axios.post(API_END_POINTS.addDataNode, req.body, {
+            ...axiosRequestConfig,
+            headers: {
+                Authorization: req.header('Authorization'),
+            },
+        })
+        res.status(response.status).send(response.data)
+    } catch (err) {
+        res.status((err && err.response && err.response.status) || 500).send(
+            (err && err.response && err.response.data) || {
+                error: unknownError,
+            }
+        )
+    }
+})
+
+fracApi.post('/addDataNodeBulk', async (req, res) => {
+    try {
+        const response = await axios.post(API_END_POINTS.addDataNodeBulk, req.body, {
             ...axiosRequestConfig,
             headers: {
                 Authorization: req.header('Authorization'),

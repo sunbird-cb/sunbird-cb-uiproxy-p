fe8ca3130cc888423bf625a8b519bfc6e31a3759
haridas.kakunje <haridas.kakunje@tarento.com>
2021-05-18 18:46:42 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -eo pipefail
 
 build_tag=$1
-name=$2
-node=$3
-org=$4
+name='sun-ui-proxies'
+node=$2
+org=$3
 
 docker build -f Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json

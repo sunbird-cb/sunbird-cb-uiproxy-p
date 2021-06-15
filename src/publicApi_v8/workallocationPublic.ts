ed9e374702850d8bb73a5709e09776d2631d1e93
amit-kumaryadav <amtydv630@gmail.com>
2021-06-15 14:03:48 +0530

    Published pdf access through QR code

--- src/publicApi_v8/workallocationPublic.ts
+++ src/publicApi_v8/workallocationPublic.ts
@@ -5,16 +5,15 @@ import { CONSTANTS } from '../utils/env'
 import { logError } from '../utils/logger'
 import { ERROR } from '../utils/message'
 const API_END_POINTS = {
-    getWAPdf: (userId: string, waId: string) => `${CONSTANTS.SB_EXT_API_BASE_2}/v1/workallocation/getWAPdf/${userId}/${waId}`,
+    getWAPdf: (waId: string) => `${CONSTANTS.SB_EXT_API_BASE_2}/getWOPublishedPdf/${waId}`,
 }
 
 export const workallocationPublic = Router()
 
-workallocationPublic.get('/getWaPdf/:userId/:waId', async (req, res) => {
+workallocationPublic.get('/getWaPdf/:waId', async (req, res) => {
     try {
-        const userId = req.params.userId
         const waId = req.params.waId
-        const response = await axios.get(API_END_POINTS.getWAPdf(userId, waId), {
+        const response = await axios.get(API_END_POINTS.getWAPdf(waId), {
             ...axiosRequestConfig,
             headers: {
             },

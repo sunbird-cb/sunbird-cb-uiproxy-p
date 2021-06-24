f302f2b9b93927e0d4cf2f230aa5d74e7aaae66e
amit-kumaryadav <amtydv630@gmail.com>
2021-06-24 11:21:32 +0530

    create user without email invitation API

--- src/protectedApi_v8/user/profile-details.ts
+++ src/protectedApi_v8/user/profile-details.ts
@@ -496,6 +496,97 @@ profileDeatailsApi.post('/createUserV2WithoutRegistry', async (req, res) => {
     }
 })
 
+// tslint:disable-next-line
+profileDeatailsApi.post('/createUserWithoutInvitationEmail', async (req, res) => {
+    try {
+        const sbChannel = req.body.personalDetails.channel
+        if (!sbChannel) {
+            res.status(400).send(channelParamMissing)
+            return
+        }
+        const sbemail_ = req.body.personalDetails.email
+        const sbemailVerified_ = true
+        const sbfirstName_ = req.body.personalDetails.firstName
+        const sblastName_ = req.body.personalDetails.lastName
+
+        const searchresponse = await axios({
+            ...axiosRequestConfig,
+            data: { request: { query: '', filters: { email: sbemail_.toLowerCase() } } },
+            method: 'POST',
+            url: API_END_POINTS.searchSb,
+        })
+        if (searchresponse.data.result.response.count > 0) {
+            res.status(400).send(emailAdressExist)
+            return
+        } else {
+            const sbUserProfile: Partial<ISBUser> = {
+                channel: sbChannel, email: sbemail_, emailVerified: sbemailVerified_, firstName: sbfirstName_,
+                lastName: sblastName_,
+            }
+            const response = await axios({
+                ...axiosRequestConfig,
+                data: { request: sbUserProfile },
+                 headers: {
+                    Authorization: CONSTANTS.SB_API_KEY,
+                    // tslint:disable-next-line: all
+                    'x-authenticated-user-token': extractUserToken(req),
+                },
+                method: 'POST',
+                url: API_END_POINTS.createSb,
+            })
+            if (response.data.responseCode === 'CLIENT_ERROR') {
+                res.status(400).send(failedToCreateUser)
+                return
+            } else {
+                const sbUserId = response.data.result.userId
+                const sbUserReadResponse = await axios({
+                    ...axiosRequestConfig,
+                    headers: {
+                        Authorization: CONSTANTS.SB_API_KEY,
+                        // tslint:disable-next-line: all
+                        'x-authenticated-user-token': extractUserToken(req),
+                    },
+                    method: 'GET',
+                    url: API_END_POINTS.userRead(sbUserId),
+                })
+                if (sbUserReadResponse.data.params.status !== 'success') {
+                    res.status(500).send(failedToReadUser)
+                    return
+                }
+
+                const personalDetailsRegistry: IPersonalDetails = {
+                    firstname: sbfirstName_,
+                    primaryEmail: sbemail_,
+                    surname: sblastName_,
+                    userName: sbUserReadResponse.data.result.response.userName,
+                }
+                const userRegistry = getUserRegistry(personalDetailsRegistry, sbChannel)
+                const userRegistryResponse = await axios({
+                    ...axiosRequestConfig,
+                    data: userRegistry,
+                    headers: {
+                        wid: sbUserId,
+                    },
+                    method: 'POST',
+                    url: API_END_POINTS.createOSUserRegistry(sbUserId),
+                })
+                if (userRegistryResponse.data === null) {
+                    res.status(500).send(failedToCreateUserInOpenSaber)
+                } else {
+                    const sbUserProfileResponse: Partial<ISunbirdbUserResponse> = {
+                        email: sbemail_, firstName: sbfirstName_, lastName: sblastName_,
+                        userId: sbUserId,
+                    }
+                    res.send(sbUserProfileResponse)
+                }
+            }
+        }
+    } catch (err) {
+        logError(createUserFailed, err)
+        res.status((err && err.response && err.response.status) || 500).send(err)
+    }
+})
+
 function getUserRegistry(personalDetailsRegistry: IPersonalDetails, deptName: string) {
     return {
         academics: [
